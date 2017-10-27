2c844467040813f2c9d4f4d37b7a109318191378
Max Isom <codetheweb@icloud.com>
2017-10-27 17:00:07 -0500

    Add JSDoc

--- index.js
+++ index.js
@@ -8,21 +8,36 @@ const strEncode = require('str-encode');
 // Import requests for devices
 const requests = require('./requests.json');
 
-// Constructor
-function TuyaDevice(params) {
+/**
+* Represents a Tuya device.
+* @constructor
+* @param {Object} options - options for constructing a TuyaDevice
+* @param {string} [options.type='outlet'] - type of device
+* @param {string} options.ip - IP of device
+* @param {number} [options.port=6668] - port of device
+* @param {string} options.id - ID of device (called `devId` or `gwId`)
+* @param {string} options.uid - UID of device
+* @param {string} options.key - encryption key of device (called `localKey`)
+* @param {number} [options.version=3.1] - protocol version
+*/
+function TuyaDevice(options) {
   // Init properties
-  this.type = params.type || 'outlet';
-  this.ip = params.ip;
-  this.port = params.port || 6668;
-  this.id = params.id;
-  this.uid = params.uid;
-  this.key = params.key;
-  this.version = params.version || 3.1;
+  this.type = options.type || 'outlet';
+  this.ip = options.ip;
+  this.port = options.port || 6668;
+  this.id = options.id;
+  this.uid = options.uid;
+  this.key = options.key;
+  this.version = options.version || 3.1;
 
   // Create cipher object
   this.cipher = forge.cipher.createCipher('AES-ECB', this.key);
 }
 
+/**
+* Gets the device's current status.
+* @param {function(error, result)} callback
+*/
 TuyaDevice.prototype.getStatus = function (callback) {
   // Add data to command
   if ('gwId' in requests[this.type].status.command) {
@@ -48,6 +63,11 @@ TuyaDevice.prototype.getStatus = function (callback) {
   });
 };
 
+/**
+* Sets the device's status.
+* @param {boolean} on - `true` for on, `false` for off
+* @param {function(error, result)} callback - returns `true` if the command succeeded
+*/
 TuyaDevice.prototype.setStatus = function (on, callback) {
   const thisRequest = requests[this.type][on ? 'on' : 'off'];
 
@@ -94,6 +114,12 @@ TuyaDevice.prototype.setStatus = function (on, callback) {
   });
 };
 
+/**
+* Sends a query to the device.
+* @private
+* @param {Buffer} buffer - buffer of data
+* @param {function(error, result)} callback
+*/
 TuyaDevice.prototype._send = function (buffer, callback) {
   // The local services of devices seem to be a bit flakey, so we'll retry the connection a couple times
   retryConnect.to({port: 6668, host: this.ip}, (error, client) => {
