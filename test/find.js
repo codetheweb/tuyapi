01c083f59d237861d1684b4ba1301840ee65ca84
Colin Kuebler <kueblc@gmail.com>
2019-02-21 14:56:59 -0500

    Fix test find returns if both id and ip are set, did not previously include ip Added test for find by ip as well as find by id

--- test/find.js
+++ test/find.js
@@ -8,7 +8,7 @@ const stub = new TuyaStub({id: '22325186db4a2217dc8e',
                            key: '4226aa407d5c1e2b',
                            state: {1: false, 2: true}});
 
-test.serial('find device on network', async t => {
+test.serial('find device on network by id', async t => {
   const stubDevice = new TuyAPI({id: '22325186db4a2217dc8e',
                                  key: '4226aa407d5c1e2b'});
   const thisStub = clone(stub);
@@ -24,8 +24,25 @@ test.serial('find device on network', async t => {
   t.not(stubDevice.device.ip, undefined);
 });
 
+test.serial('find device on network by ip', async t => {
+  const stubDevice = new TuyAPI({ip: 'localhost',
+                                 key: '4226aa407d5c1e2b'});
+  const thisStub = clone(stub);
+  thisStub.startServer();
+
+  thisStub.startUDPBroadcast({interval: 1});
+
+  await stubDevice.find();
+
+  stubDevice.disconnect();
+  thisStub.shutdown();
+
+  t.not(stubDevice.device.id, undefined);
+});
+
 test.serial('find returns if both ID and IP are already set', async t => {
-  const stubDevice = new TuyAPI({id: '22325186db4a2217dc8e',
+  const stubDevice = new TuyAPI({ip: 'localhost',
+                                 id: '22325186db4a2217dc8e',
                                  key: '4226aa407d5c1e2b'});
   const thisStub = clone(stub);
   thisStub.startServer();
