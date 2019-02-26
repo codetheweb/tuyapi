31a0cabf76149e29391b08409f9be1bfa7cabc0c
Colin Kuebler <kueblc@gmail.com>
2019-02-26 18:54:36 -0500

    Parser cipher updates (#160)
    * Fixed a few parsing issues:
    zero-padding is actually the return code from the device where 0 = success
    Added _returnCode, _crc to MessageParser
    (reordered to match order in memory)
    (crc is not actually checked here, but it could be!)
    More restrictive length checks, taking into account _returnCode and _crc
    _commandByte is a uint32
    Fill _leftOver with, well, leftovers
    Reordered parse logic to match order in memory
    (this also allows us to extract more data in case of a suffix mismatch)
    suffix indexed relative to _payloadSize, relevant in the case of leftovers
    (which happens when two messages are sent in one)
    
    These changes were made with the best attempt to adhere to the existing coding style
    I would personally make parse and encode both static functions
    We could check the crc value for data integrity
    If _leftOver is populated, better try decoding that too
    
    A more drastic overhaul can be found:
    https://github.com/kueblc/mocktuyacloud/blob/master/lib/lan-frame.js
    But no attempt has been made to maintain the previous coding style
    This adaptation was made to integrate seamlessly into the existing tuyapi project
    
    * Reimplemented encrypt, decrypt, and md5 using built in crypto library
    Removes dependency on third party node-forge library
    Clarified md5 documentation
    
    * Default encrypt options.base64 to true
    
    * Fix lint no-negated-condition
    
    * Cleaned up _encode implementation using one buffer to assemble payload and remove string representations of binary data
    
    * Fixed test for encrypting with base64 option disabled
    Had previously made a poor assumption about the meaning of the flag and hadn't realized utf8 is not binary safe
    
    * Fixed cipher.encrypt base64 option, now correctly outputs a Buffer when base64 = false
    
    * Throw appropriate errors when parsing fails
    Replaced 16 with a constant HEADER_SIZE for better code readability
    Cleverly account for return code or lack thereof by checking if the first 3 bytes are zero, which they often (always?) are for return code
    Moving forward, we should decide whether this parser should serve both client and device emulation
    
    * Removed unused _parsed flag
    
    * Added tests for message-parser, bringing coverage to 100
    Test decoding a plaintext payload, as is the case when a device reports an error
    Test for several types of corrupt messages; short, way too short, prefix and suffix mismatch
    Test handling of two packets coming through at the same time
    (for the time being, we just don't catch the second message; this should change)
    
    * Removed unused crc package from dependencies
    
    * Move clone from prod dependencies to dev dependencies
    
    * Moved parse and encode from static wrapped Class methods into static functions, skipping the middle man
    This change touched a lot of lines due to indentation and a few renames but code was otherwise unmodified
    More cleanup to come once we move on to breaking changes
    (ie its possible to receive multiple packets in one buffer, should therefore change parse to return an array of parsed packets)
    (this will require touching index.js and so is left for another revision)
    We can also return more information about the packet than we currently are
    (ie sequence index, return code)
    We can also generate and check CRCs (to be ported from MockTuyaCloud)

--- test/parser.js
+++ test/parser.js
@@ -21,6 +21,16 @@ test('decode empty message', t => {
   t.falsy(parsed.data);
 });
 
+test('decode message where payload is not a JSON object', t => {
+  const payload = 'gw id invalid';
+
+  const encoded = Parser.encode({data: payload, commandByte: '0a'});
+
+  const parsed = Parser.parse(encoded);
+
+  t.deepEqual(payload, parsed.data);
+});
+
 test('decode corrupt (shortened) message', t => {
   const payload = {devId: '002004265ccf7fb1b659', dps: {1: true, 2: 0}};
 
@@ -29,4 +39,46 @@ test('decode corrupt (shortened) message', t => {
   t.throws(() => {
     Parser.parse(encoded.slice(0, -10));
   });
+});
+
+test('decode corrupt (shorter than possible) message', t => {
+  const payload = {devId: '002004265ccf7fb1b659', dps: {1: true, 2: 0}};
+
+  const encoded = Parser.encode({data: payload, commandByte: '0a'});
+
+  t.throws(() => {
+    Parser.parse(encoded.slice(0, 23));
+  });
+});
+
+test('decode corrupt (prefix mismatch) message', t => {
+  const payload = {devId: '002004265ccf7fb1b659', dps: {1: true, 2: 0}};
+
+  const encoded = Parser.encode({data: payload, commandByte: '0a'});
+  encoded.writeUInt32BE(0xDEADBEEF, 0);
+
+  t.throws(() => {
+    Parser.parse(encoded);
+  });
+});
+
+test('decode corrupt (suffix mismatch) message', t => {
+  const payload = {devId: '002004265ccf7fb1b659', dps: {1: true, 2: 0}};
+
+  const encoded = Parser.encode({data: payload, commandByte: '0a'});
+  encoded.writeUInt32BE(0xDEADBEEF, encoded.length - 4);
+
+  t.throws(() => {
+    Parser.parse(encoded);
+  });
+});
+
+test('decode message with two packets', t => {
+  const payload = {devId: '002004265ccf7fb1b659', dps: {1: true, 2: 0}};
+
+  const encoded = Parser.encode({data: payload, commandByte: '0a'});
+
+  const parsed = Parser.parse(Buffer.concat([encoded, encoded]));
+  t.deepEqual(parsed.data, payload);
+  t.deepEqual(parsed.commandByte, 10);
 });
